4051468ccae64788cd7760c5da8bcf9d89685309
IndraHfid <indrahafidsaputra@gmail.com>
2025-10-16 15:51:21 +0700

    add centered text in second Box

--- app/src/main/java/com/example/indrahafid/TataLetak.kt
+++ app/src/main/java/com/example/indrahafid/TataLetak.kt
@@ -166,12 +166,17 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 contentDescription = null,
                 contentScale = ContentScale.Fit
             )
-
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier
+                    .align(Alignment.Center)
+            )
         }
-
-
     }
-
 }
 
 
